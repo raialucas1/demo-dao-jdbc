31b4d207e8e91d94c6f83a52f201fdf8adc5fdb6
raialucas1 <raialucas@hotmail.com>
2024-03-08 09:15:02 -0300

    Deparment entity class

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department obj= new Department(4,"Books");
+		
+		System.out.println(obj);
+
+	}
+
+}
