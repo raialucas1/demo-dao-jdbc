38bd72502c7b3e15c0df3a03abe341ebcff23588
raialucas1 <raialucas@hotmail.com>
2024-03-09 21:31:21 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -30,6 +30,11 @@ public class Program {
         for(Seller obj:list) {
         	System.out.println(obj);
         }
+        System.out.println("### TESTE 4: seller insert ###");
+        Seller newSeller=new Seller(null,"Raia","Raia@gmail",new Date(),4000.0,department);
+        sellerDao.insert(newSeller);
+        System.out.println(newSeller);
+
 	}
 
 }
